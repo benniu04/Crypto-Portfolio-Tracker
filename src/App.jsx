a2fb8377287e98fa565db8dadbb5c2aa4495e332
Benjamin <138111756+benniu04@users.noreply.github.com>
2025-01-22 18:38:53 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    const [portfolio, setPortfolio] = useState([]);
+
+    return (
+        <div>
+            <h1>Crypto Portfolio Tracker</h1>
+        </div>
+    )
+}
+
+export default App;
